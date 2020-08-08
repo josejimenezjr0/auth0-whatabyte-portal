205e563ad8c222578bc50ff2dcfb96146572f48b
Jose Jimenez Jr <josejimenezjr0@gmail.com>
2020-08-08 16:11:43 -0500

    add secured middleware to secure protected routes

--- index.js
+++ index.js
@@ -98,12 +98,24 @@ app.use((req, res, next) => {
  * Routes Definitions
  */
 
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+};
+
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
 
-app.get("/user", (req, res) => {
-  res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
+app.get("/user", secured, (req, res, next) => {
+  const { _raw, _json, ...userProfile } = req.user;
+  res.render("user", {
+    title: "Profile",
+    userProfile: userProfile
+  });
 });
 
 /**
